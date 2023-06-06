c512f495c9f7bfa9b00129c58ea861d1eca24dac
th3Ryo <fabian_b.g@hotmail.com>
2023-06-06 13:59:19 -0500

    primer commit, es un div vacio intento 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className='text-3xl font-bold underline bg-red-100'>
-       esto es un div vacio
+       esto es un div vacio intento 2
       </div>
       
     </>
