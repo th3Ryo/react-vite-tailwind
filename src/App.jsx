70af9e854b9c1a42b35e851267fe0d512d376d18
th3Ryo <fabian_b.g@hotmail.com>
2023-06-06 13:37:55 -0500

    primer commit, es un div vavio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline bg-red-100'>
+       esto es un div vacio
+      </div>
+      
+    </>
+  )
+}
+
+export default App
