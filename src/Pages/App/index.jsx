8171896977a7b5c888eb510939323371abc1a111
th3Ryo <fabian_b.g@hotmail.com>
2023-06-06 14:51:29 -0500

    2 commit, estructura base de la tienda con componentes

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,30 @@
+import {Home} from "../Home"
+import {MyAccount} from "../MyAccount"
+import {MyOrder} from "../MyOrder"
+import {MyOrders} from "../MyOrders"
+import {NotFound} from "../NotFound"
+import {SignIn} from "../SignIn"
+
+
+import './App.css'
+
+    
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline bg-red-100'>
+        <Home />
+        <MyAccount />
+        <MyOrder />
+        <MyOrders />
+        <NotFound />
+        <SignIn />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
